006df0048b318cc9a49b7d59d2b31ec390d1b874
drewkates <70718949+drewkates@users.noreply.github.com>
2020-10-17 17:16:12 -0400

    Update User.java
    I undid deleting stuff

--- app/src/main/java/com/example/moodtrackerhw2020/User.java
+++ app/src/main/java/com/example/moodtrackerhw2020/User.java
@@ -4,6 +4,61 @@ public class User {
 
     private String userName;
     private final String password;
+    private String entryLog;
+
+    private Happy happy;
+    private Sad sad;
+    private Tired tired;
+    private Stressed stressed;
+    private Angry angry;
+
+    public String getEntryLog() {
+        return entryLog;
+    }
+
+    public void setEntryLog(String entryLog) {
+        this.entryLog = entryLog;
+    }
+
+    public Happy getHappy() {
+        return happy;
+    }
+
+    public void setHappy(Happy happy) {
+        this.happy = happy;
+    }
+
+    public Sad getSad() {
+        return sad;
+    }
+
+    public void setSad(Sad sad) {
+        this.sad = sad;
+    }
+
+    public Tired getTired() {
+        return tired;
+    }
+
+    public void setTired(Tired tired) {
+        this.tired = tired;
+    }
+
+    public Stressed getStressed() {
+        return stressed;
+    }
+
+    public void setStressed(Stressed stressed) {
+        this.stressed = stressed;
+    }
+
+    public Angry getAngry() {
+        return angry;
+    }
+
+    public void setAngry(Angry angry) {
+        this.angry = angry;
+    }
 
     public User(String userName, String password) {
         this.password = password;
