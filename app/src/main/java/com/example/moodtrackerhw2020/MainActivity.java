76af39fe120f1ecf4ffb5f517450b4fd0c1d56c6
drewkates <70718949+drewkates@users.noreply.github.com>
2020-10-17 16:25:48 -0400

    Updated xml
    Updated XML and MainActivity to reflect username

--- app/src/main/java/com/example/moodtrackerhw2020/MainActivity.java
+++ app/src/main/java/com/example/moodtrackerhw2020/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.moodtrackerhw2020;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +21,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(login);
         }
     }
+
 }
