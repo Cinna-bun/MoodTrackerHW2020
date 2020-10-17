93ef160ecbb92db8900186c745455281ebd916e6
SofiMeza <meza8@purdue.edu>
2020-10-17 12:27:03 -0600

    Added MoodHistory

---
+++ app/src/main/java/com/example/moodtrackerhw2020/MoodHistory.java
@@ -0,0 +1,14 @@
+package com.example.moodtrackerhw2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoodHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_history);
+    }
+}
