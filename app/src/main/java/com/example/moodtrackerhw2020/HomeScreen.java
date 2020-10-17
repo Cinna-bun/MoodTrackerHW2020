962c6501cab896b54976bcbd2dcaf3c911d0d778
Michael <mpskarate@hotmail.com>
2020-10-17 16:37:29 -0400

    added some spinner logic

--- app/src/main/java/com/example/moodtrackerhw2020/HomeScreen.java
+++ app/src/main/java/com/example/moodtrackerhw2020/HomeScreen.java
@@ -3,10 +3,18 @@ package com.example.moodtrackerhw2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 
 public class HomeScreen extends AppCompatActivity {
+    //TODO make fields for the four emotions
+    private Spinner happyScale;
+    private Spinner sadScale;
+    private Spinner angryScale;
+    private Spinner tiredScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +22,10 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
         //making the spinners to get the numbers from the users
-        Spinner happyScale = (Spinner) findViewById(R.id.happySpinner);
-        Spinner sadScale = (Spinner) findViewById(R.id.sadSpinner);
-        Spinner angryScale = (Spinner) findViewById(R.id.angrySpinner);
-        Spinner tiredScale = (Spinner) findViewById(R.id.tiredSpinner);
+        happyScale = (Spinner) findViewById(R.id.happySpinner);
+        sadScale = (Spinner) findViewById(R.id.sadSpinner);
+        angryScale = (Spinner) findViewById(R.id.angrySpinner);
+        tiredScale = (Spinner) findViewById(R.id.tiredSpinner);
 
         //this activates the spinners above
         ArrayAdapter<String> numberRatingAdapter = new ArrayAdapter<String>(HomeScreen.this,
@@ -29,5 +37,16 @@ public class HomeScreen extends AppCompatActivity {
         tiredScale.setAdapter(numberRatingAdapter);
     }
 
+    /**
+     * Gets the user's current happy rating
+     * @return the
+     */
+    public int getHappyRating() {
+        String text = happyScale.getSelectedItem().toString();
+        int result = Integer.parseInt(text);
 
+        return result;
+    }
+
+    
 }
