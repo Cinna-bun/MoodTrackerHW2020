4403ccb26e284d8f20ff61d93835a83a1a1a5741
Michael <mpskarate@hotmail.com>
2020-10-17 14:55:15 -0400

    Added working buttons

---
+++ app/src/main/java/com/example/moodtrackerhw2020/Emotion.java
@@ -0,0 +1,8 @@
+package com.example.moodtrackerhw2020;
+
+public abstract class Emotion {
+    int scale;
+    String name;
+    String description;
+
+}
--- app/src/main/java/com/example/moodtrackerhw2020/HomeScreen.java
+++ app/src/main/java/com/example/moodtrackerhw2020/HomeScreen.java
@@ -3,6 +3,8 @@ package com.example.moodtrackerhw2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //making the spinners to get the numbers from the users
+        Spinner happyScale = (Spinner) findViewById(R.id.happySpinner);
+        Spinner sadScale = (Spinner) findViewById(R.id.sadSpinner);
+        Spinner angryScale = (Spinner) findViewById(R.id.angrySpinner);
+        Spinner tiredScale = (Spinner) findViewById(R.id.tiredSpinner);
+
+        ArrayAdapter<String> numberRatingAdapter = new ArrayAdapter<String>(HomeScreen.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.numbers));
+        numberRatingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        happyScale.setAdapter(numberRatingAdapter);
+        sadScale.setAdapter(numberRatingAdapter);
+        angryScale.setAdapter(numberRatingAdapter);
+        tiredScale.setAdapter(numberRatingAdapter);
     }
+
+
 }
